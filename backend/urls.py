ec5ddcf31e44ecc2758ea23897abfe9b6ade6021
sebas2007mars1 <sebas2007mars1@gmail.com>
2026-09-15 15:36:31 -0500

    Resuelve conflicto en urls

--- backend/urls.py
+++ backend/urls.py
@@ -1,24 +1,7 @@
-"""
-URL configuration for backend project.
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/6.1/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 
-from backend.usuarios.views import login_usuario, inicio
-from backend.usuarios.views_password import (
+from usuarios.views_password import (
     RecuperarPasswordView,
     RecuperarEnviadoView,
     PasswordResetConfirmView,
@@ -28,9 +11,8 @@ from backend.usuarios.views_password import (
 urlpatterns = [
     path("admin/", admin.site.urls),
 
-    # Login e inicio
-    path("login/", login_usuario, name="login"),
-    path("inicio/", inicio, name="inicio"),
+    # Rutas existentes del proyecto
+    path("", include("usuarios.urls")),
 
     # Recuperación de contraseña
     path(
